52be9154082e2cf1fd816f09f1ce95c7d81b4ef7
matheusvmg <matheusvmg@gmail.com>
2020-07-02 02:14:33 -0300

    antes da atualização de correção de bugs

--- app/styles/CardImc.js
+++ app/styles/CardImc.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 //estilização
 const styles = StyleSheet.create({
